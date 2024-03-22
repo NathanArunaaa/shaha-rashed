c4dda9a3ff8c5fd95ba07d64b081ef1a807d39d1
NathanArunaaa <nathanaruna07@gmail.com>
2024-03-22 13:08:50 -0400

    test1

---
+++ main.py
@@ -0,0 +1,38 @@
+
+import serial
+import time
+import RPi.GPIO as GPIO
+
+
+def pumpOn():
+    relay1 = 23
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(relay1, GPIO.OUT)
+    GPIO.output(relay1, GPIO.LOW)
+    
+def pumpOff():
+    relay1 = 23
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(relay1, GPIO.OUT)
+    GPIO.output(relay1, GPIO.HIGH)
+
+time.sleep(1)
+    
+while True:
+    
+    ser = serial.Serial('/dev/ttyACM0', 9600)
+    data = ser.readline().decode('latin-1').strip()
+    print(data)
+    
+    if data >=  400:
+        pumpOn()
+        
+    if data =< 300:
+        pumpOff()
+        
+    else:
+        print("error ")
+    
+
+ 
+
